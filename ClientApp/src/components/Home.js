b5e485642574c8880fc4859561b1a2b16293737f
Dea Fetahu <df56156@ubt-uni.net>
2023-04-24 23:36:02 +0200

    home-page ndryshimet

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,15 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import './Home.css';
 
+
+
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
       return (
-         <Fragment>
+          <Fragment >
           <div className='home-container'>
               <video src="videoHome.mp4" autoPlay loop muted />
-              <h1><span>Sun<span className='rise'>Rise</span>Care Hospital</span></h1>
+              <h1><span>SunRiseCare Hospital</span></h1>
               <p><strong>Welcome to our Hospital, where your health is our priority !</strong></p>
               
               </div>
@@ -53,7 +55,31 @@ export class Home extends Component {
                       </div>
                   </div>
               </div>
-         
+              <div className='all-buttons'>
+              <div className='container-button'>
+                      <div className='button-1'><strong><a>Patient Registration</a></strong></div>
+              </div>
+              <div className='container-button'>
+                      <div className='button-1'><strong><a>Choose an appointment</a></strong></div>
+                  </div>
+               <div className='container-button'>
+                      <div className='button-1'><strong>Find our location</strong></div>
+               </div>
+                  <div className='container-button'>
+                      <div className='button-1'><strong><a>Contact</a></strong></div>
+                  </div>
+              </div>
+              <hr></hr>
+              <h1>Where can you find us ?</h1>
+              <br></br>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2856.812469825241!2d21.143523503176603!3d42.65200171275375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ef3f69baacb%3A0xf864a269cc75e908!2sDukagjini%20Residence!5e0!3m2!1sen!2s!4v1682282589650!5m2!1sen!2s"
+                  width="600"
+                  height="450"
+                  style={{ border: "0" }}
+                  allowfullscreen=""
+                  loading="lazy">
+              </iframe>
+
         </Fragment>
       );
   }
