7661d53658ca826003be6ddd6de667fa5908aebb
elsatomaj <elsatomaj173@gmail.com>
2023-04-24 22:45:53 +0200

    services almost done edhe pak sene kom me shtu edhe fotot mi ndrru

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,31 +1,292 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './sherbimet.css';
+
 
 export class Counter extends Component {
-  static displayName = Counter.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
 
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
+    render() {
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
+        return (
+            <Fragment>
+                <div className='sherbimet-container'>
+                    <h1>SHERBIMET MJEKESORE</h1>
+                </div>
 
-        <p>This is a simple example of a React component.</p>
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+                <div className='slider'>
+                </div>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+
+                <div className='h1'>
+                    <h1>Caring for you, every step of the way.</h1>
+                </div>
+
+                <div className='wrapper'>
+                    <div className='row'>
+                        <div className='image-section'>
+                            <img src="about.jpg"></img>
+
+                        </div>
+
+                        <div className='content'>
+                            <h1>Sherbimet</h1>
+                            <h2>Our Hospital</h2>
+                            <p>Sherbimet ne spitalin tone jane te rendesishme per shendetin dhe mireqenien e pacienteve tane.
+                                Spitali yne eshte i perkushtuar per ofrimin e kujdesit shendetesor cilesor dhe per te permiresuar sherbimet tona me teknologjine
+                                dhe pajisjet me te fundit, si dhe me stafin mjekesor te trajnuar dhe te kualifikuar.
+                                Sherbimet ne spitalin tone perfshijne trajtimin emergjent te semundjeve dhe lendimeve akute, diagnozen dhe trajtimin e semundjeve te ndryshme, kujdesin per pacientet ne kujdesin intensiv, rehabilitimin, kujdesin palliativ dhe sherbimet kirurgjikale. Spitali yne gjithashtu ofron sherbime te specializuara per te trajtuar semundje dhe gjendje te caktuara shendetesore, siç jane kardiologjia, neorologjia, onkologjia dhe ortopedia.
+
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className='flexbox'>
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="endocrinology.jpeg"></img>
+                        </div>
+                        <p className='title'>Endocrinology</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="gynecology.jpg"></img>
+                        </div>
+                        <p className='title'>Gjinekologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="alergology.jpg"></img>
+                        </div>
+                        <p className='title'>Alergologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="anesthesiology.jpg"></img>
+                        </div>
+                        <p className='title'>Anesteziologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="checkuup.jpg"></img>
+                        </div>
+                        <p className='title'>CheckUp</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="dermatology.jpg"></img>
+                        </div>
+                        <p className='title'>Dermatologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="gastroenterology.jpg"></img>
+                        </div>
+                        <p className='title'>Gastroenterologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="hematology.jpg"></img>
+                        </div>
+                        <p className='title'>Hematologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="invitro.jpg"></img>
+                        </div>
+                        <p className='title'>Fekondimi in Vitro</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="cardiocirurgy.jpg"></img>
+                        </div>
+                        <p className='title'>Kardiokirurgji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="cardiology.jpg"></img>
+                        </div>
+                        <p className='title'>Kardiologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="pediatri.jpg"></img>
+                        </div>
+                        <p className='title'>Pediatri</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="laborator.jpg"></img>
+                        </div>
+                        <p className='title'>Laborator</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="kirurgji.jpg"></img>
+                        </div>
+                        <p className='title'>Kirurgji e pergjithshme</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="kirurgjiplastike.jpg"></img>
+                        </div>
+                        <p className='title'>Kirurgji Platike</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="nefrologji.jpeg"></img>
+                        </div>
+                        <p className='title'>Nefrologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="neurologji.jpg"></img>
+                        </div>
+                        <p className='title'>Neurologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="neurok.jpg"></img>
+                        </div>
+                        <p className='title'>Neurokirurgji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="okulistik.jpg"></img>
+                        </div>
+                        <p className='title'>Okulistike</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="orl.jpg"></img>
+                        </div>
+                        <p className='title'>ORL</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="oncology.jpg"></img>
+                        </div>
+                        <p className='title'>Onkologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="pleumonogy.jpg"></img>
+                        </div>
+                        <p className='title'>Pneumologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="urology.jpg"></img>
+                        </div>
+                        <p className='title'>Urologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="reuma.jpg"></img>
+                        </div>
+                        <p className='title'>Reumatologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="urgjenca.jpg"></img>
+                        </div>
+                        <p className='title'>Urgjenca 24h</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="proct.jpg"></img>
+                        </div>
+                        <p className='title'>Proktologji</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="salla.jpg"></img>
+                        </div>
+                        <p className='title'>Sallat e lindjes</p>
+                        <a href="#">Read More...</a>
+                    </div>
+
+                    <div className='card'>
+                        <div className='img'>
+                            <img src="qendra.jpg"></img>
+                        </div>
+                        <p className='title'>Qendra e Dializes</p>
+                        <a href="#">Read More...</a>
+                    </div>
+            </div>
+          
+
+            </Fragment>
+        );
+
+
+
+    }
 }
