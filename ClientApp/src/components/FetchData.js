4f1b06c20298d5519d8737aafa259b9b5decba4f
MarjetaRamaj <marjeta.r12@gmail.com>
2023-04-26 00:43:08 +0200

    contact forma u perfundu

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,59 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './contact.css';
 
 export class FetchData extends Component {
-  static displayName = FetchData.name;
+    render() {
+        return (
+            <Fragment>
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+                <div className="container">
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+                    <span class="big-circle"></span>
+                        
+                    <div className="form">
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+                        <div className="contact-info">
+                            <h3 className="title">Let's get in touch</h3>
+                            <p className="text">Should you have any questions or concerns - or
+                                simply want more information about Sunrisecare Hospital,
+                                please feel free to contact us.</p>
+                            <p>We would love to hear from you!</p> <br />
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+                            <div className="info">
+                                <div className="information">
+                                    <img src="point.png" className="icon" alt="" />
+                                    <p>Prishtine, 10000</p>
+                                </div>
+                                <div className="information">
+                                    <img src="email.png" className="icon" alt="" />
+                                    <p>sunrisecare.info@gmail.com</p>
+                                </div>
+                                <div className="information">
+                                    <img src="phone.png" className="icon" alt="" />
+                                    <p>(+383) 48-136-910</p>
+                                </div>
+                            </div>
 
-    return (
-      <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
+                            <div className="social-media">
+                                <p><b>Connect with us:</b></p>
+                                <div className="social-icons">
+                                    <a href="https://www.facebook.com/">
+                                    <img src="facebook.png" className="icon1" alt="" />
+                                    </a>
+                                    <a href="https://www.instagram.com/">
+                                    <img src="instagram.png" className="icon1" alt="" />
+                                    </a>
+                                    <a href="#">
+                                    <img src="twitter.png" className="icon1" alt="" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+                        <div className="contact-form">
+                            <form>
+                                <h2><b>Contact Us</b></h2>
+                                <input type="text" className="field" placeholder="Name" id="name" />
+
+                                <input type="email" className="field" placeholder="Email" id="email" />
+
+                                <input type="text" className="field" placeholder="Number" id="telefoni" />
+
+                                <textarea className="field area" placeholder="Message" id="comment"></textarea>
+
+                                <input type="submit" value="Send" className="button" />
+                                <div className="Login">
+                                    You haven't been logged in yet? <a href="./LogIn.html"> Login here</a>
+                                </div>
+
+                            </form>
+                        </div>
+
+                    </div> <br/> 
+                </div>
+            </Fragment>
+
+        );
+    }
 }
