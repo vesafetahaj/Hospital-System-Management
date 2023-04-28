6df2a432b784e4ff1e2cc20a2f29ef8a0398e493
vesafetahaj <vf58160@ubt-uni.net>
2023-04-28 21:29:24 +0200

    NavBar active pages

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,64 +1,81 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+    import React, { Component } from 'react';
+    import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+    import { Link } from 'react-router-dom';
+    import './NavMenu.css';
+
+ 
 
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+    export class NavMenu extends Component {
+      static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+      constructor (props) {
+        super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+          collapsed: true
+          };
+          this.state = {
+              activeLink: ""
+          };
+      }
+        
+       
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+      toggleNavbar () {
+        this.setState({
+          collapsed: !this.state.collapsed
+        });
+      }
 
-  render() {
-    return (
-      <header>
-            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                <NavbarBrand tag={Link} to="/">
-                    <div style={{ maxWidth: "350px" }}>
-                    <img src="logo-no-background.png" alt='Logo' id="logoSpitalit"/>
-                </div></NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                   <NavLink tag={Link} className="text-dark" to="/sherbimet">Services</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/aboutus">About Us</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/contactus" >Contact Us</NavLink>
-              </NavItem>
-              <NavItem>
-                 <NavLink tag={Link} className="text-dark" to="/login" id="last">Log In</NavLink>
-              </NavItem>
-             </ul>
+      render() {
+        return (
+          <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                    <NavbarBrand tag={Link} to="/">
+                        <div style={{ maxWidth: "350px" }}>
+                        <img src="logo-no-background.png" alt='Logo' id="logoSpitalit"/>
+                    </div></NavbarBrand>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                <ul className="navbar-nav flex-grow">
+                  <NavItem>
+                                <NavLink tag={Link} className={this.state.activeLink === "/" ? "text-dark active" : "text-dark"}
+                                    to="/"
+                                    onClick={() => this.setState({ activeLink: "/" })}>Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                                <NavLink tag={Link} className={this.state.activeLink === "/sherbimet" ? "text-dark active" : "text-dark"}
+                                    to="/sherbimet"
+                                    onClick={() => this.setState({ activeLink: "/sherbimet" })}>Services</NavLink>
+                  </NavItem>
+                  <NavItem>
+                                <NavLink tag={Link} className={this.state.activeLink === "/aboutus" ? "text-dark active" : "text-dark"}
+                                    to="/aboutus"
+                                    onClick={() => this.setState({ activeLink: "/aboutus" })}>About Us</NavLink>
+                  </NavItem>
+                  <NavItem>
+                                <NavLink tag={Link} className={this.state.activeLink === "/contactus" ? "text-dark active" : "text-dark"}
+                                    to="/contactus"
+                                    onClick={() => this.setState({ activeLink: "/contactus" })} >Contact Us</NavLink>
+                  </NavItem>
+                  <NavItem>
+                                <NavLink tag={Link} className={this.state.activeLink === "/login" ? "text-dark active" : "text-dark"}
+                                    to="/login"
+                                    onClick={() => this.setState({ activeLink: "/login" })} id="last">Log In</NavLink>
+                  </NavItem>
+                 </ul>
               
-                </Collapse>
+                    </Collapse>
 
                 
                
 
-            </Navbar>
+                </Navbar>
 
-        </header>
+            </header>
 
-    );
-  }
-}
+        );
+      }
+    }
